48f6a86aa1ab4171e15cd81c201fb011f49a072f
Simon Bernardin <simon.bernardin@moventes.com>
2018-04-13 16:42:30 +0200

    second iteration

---
+++ app/src/main/java/com/example/imdb/imdb/DetailsActivity.java
@@ -0,0 +1,24 @@
+package com.example.imdb.imdb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.details_activity);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(ListActivity.DETAILS_ACTIVITY_PARAM);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.title);
+        textView.setText(message);
+    }
+}
--- app/src/main/java/com/example/imdb/imdb/ListActivity.java
+++ app/src/main/java/com/example/imdb/imdb/ListActivity.java
@@ -1,14 +1,19 @@
 package com.example.imdb.imdb;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
+
+    public static String DETAILS_ACTIVITY_PARAM = "com.example.imdb.imdb.DETAILS_ACTIVITY_PARAM";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,5 +31,13 @@ public class ListActivity extends AppCompatActivity {
         ListAdapter adapter = new ListAdapter(this, R.layout.list_item, movies);
 
         listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent detailsActivity = new Intent(ListActivity.this, DetailsActivity.class);
+                detailsActivity.putExtra(DETAILS_ACTIVITY_PARAM, movies.get(position).getTitle());
+                ListActivity.this.startActivity(detailsActivity);
+            }
+        });
     }
 }
